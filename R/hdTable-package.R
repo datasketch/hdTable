d54bd492a895c7d120404f32cef6550b1853c5c6
Jp Marin Diaz <jpmarindiaz@gmail.com>
2023-05-18 15:09:33 -0500

    docs

--- R/hdTable-package.R
+++ R/hdTable-package.R
@@ -4,7 +4,7 @@
 ## usethis namespace: start
 #' @importFrom dstools %||%
 #' @importFrom hdtype hdtype is_hdtype as_hdtype as_basetype
-#' @importFrom hdtype Cat Dat Num Yea Pct UKT NUT
+#' @importFrom hdtype Cat Dat Num Yea Pct Bin Chk UKT NUT
 #' @importFrom R6 R6Class
 ## usethis namespace: end
 NULL
