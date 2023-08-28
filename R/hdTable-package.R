13d20d796bf0f8524fa6a702486d55c6b1294cae
Jp Marin Diaz <jpmarindiaz@gmail.com>
2023-08-27 22:37:33 -0500

    fixing dependencies

--- R/hdTable-package.R
+++ R/hdTable-package.R
@@ -5,8 +5,8 @@
 #' @importFrom dstools %||%
 #' @importFrom fs file_size
 #' @importFrom tools file_ext
-#' @importFrom hdtype hdtype is_hdtype as_hdtype as_basetype
-#' @importFrom hdtype Cat Dat Num Yea Pct Bin Chk UKT NUT
 #' @importFrom R6 R6Class
+#' @import vctrs
+#' @import hdtype
 ## usethis namespace: end
 NULL
